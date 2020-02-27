983b561f129e2717e4170a028a74776693e174c3
Mizbah Ahsan <baharam000@gmail.com>
2020-02-27 19:56:28 +0600

    initial commit

---
+++ src/NumToWordServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Media365\NumToWord;
+
+use Illuminate\Support\ServiceProvider;
+
+class NumToWordServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+        $this->app->singleton(NumToWord::class, function (){
+            return new NumToWord();
+        });
+    }
+}
